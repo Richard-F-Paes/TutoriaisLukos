f4f210a9724630370da868a5c84320cf8f13d404
rich353453 <richard.paes2003ag@gmail.com-rich353453>
2025-10-21 02:24:07 -0300

    Correção do componente do tutorial InteractiveLesson

--- src/components/InteractiveLesson/InteractiveLesson.jsx
+++ src/components/InteractiveLesson/InteractiveLesson.jsx
@@ -335,7 +335,7 @@ const InteractiveLesson = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 flex justify-center">
           {/* Main Content */}
           <div className="lg:col-span-2">
             {/* Video Player */}
